fcb8279530e549e056db082e4aba67e8c78edb59
WonHyung Lee(Gram) <link8879@naver.com>
2024-05-15 16:36:09 +0900

    주유소 가격 출력 성공

--- xmlReader.py
+++ xmlReader.py
@@ -2,38 +2,13 @@ import requests
 import xml.etree.ElementTree as ET
 api_key = '5758303589'
 
-# # url 입력
-# url = 'https://data.ex.co.kr/openapi/business/serviceAreaRoute?key='+api_key+'&type=xml&routeName='+ex_name+'&numOfRows=50&pageNo=1'
-# #url = 'https://data.ex.co.kr/openapi/specialAnal/trafficFlowByTime?key=5758303589&type=xml&iStdYear=2024'
-# # url 불러오기
-# response = requests.get(url)
-#
-# #데이터 값 출력해보기
-# print(response.text)
-# page_num = '1'
-# while True:
-#     url = 'https://data.ex.co.kr/openapi/business/serviceAreaRoute?key=' + api_key + '&type=xml&routeName=' + ex_name + '&numOfRows=10&pageNo='+ page_num
-#     print(url)
-#     response = requests.get(url)
-#     root = ET.fromstring(response.text)
-#
-#     flag = root.find("list")
-#
-#     if flag == None:
-#         break
-#
-#     for list in root.iter("list"):
-#         print(list.findtext("brand"))
-#     page_num = str(int(page_num) + 1)
-
-
-def FoodMenuReader(serviceArea_name):
+def FoodMenuReader(serviceArea_name):               # return food menu list
     page_num = '1'
     flag = False
+    menu = []
     while True:
         url = 'https://data.ex.co.kr/openapi/restinfo/restBestfoodList?key=test&type=xml&numOfRows=10&pageNo='+page_num+'&stdRestNm='+serviceArea_name
     # url 불러오기
-        response = requests.get(url)
         response = requests.get(url)
         root = ET.fromstring(response.text)
         flag = root.find("list")
@@ -41,18 +16,35 @@ def FoodMenuReader(serviceArea_name):
         if flag == None:
             break
         for list in root.iter("list"):
-            print(list.findtext("foodNm"))
+            menu.append(list.findtext("foodNm"))
         page_num = str(int(page_num)+1)
-
+    return menu
 
 
     pass
 def GasstationReader(serviceArea_name):
-    url = 'https://data.ex.co.kr/openapi/business/serviceAreaRoute?key=' + api_key + '&type=xml&routeName=' + ex_name + '&numOfRows=50&pageNo=1'
-    # url = 'https://data.ex.co.kr/openapi/specialAnal/trafficFlowByTime?key=5758303589&type=xml&iStdYear=2024'
-    # url 불러오기
-    response = requests.get(url)
-    pass
+    page_num = '1'
+    flag = False
+    oil_price = {}
+    while True:
+        url = 'https://data.ex.co.kr/openapi/business/curStateStation?key=test&type=xml&numOfRows=10&pageNo='+page_num+'&serviceAreaName='+serviceArea_name
+        # url 불러오기
+        response = requests.get(url)
+        root = ET.fromstring(response.text)
+        flag = root.find("list")
+
+        if flag == None:
+            break
+        for list in root.iter("list"):
+            company_name = list.findtext("oilCompany")
+            oil_price[company_name] = {'disel':0,'gasoline':0}
+            oil_price[company_name]['disel'] = list.findtext("diselPrice")
+            oil_price[company_name]['gasoline'] = list.findtext("gasolinePrice")
+
+        page_num = str(int(page_num) + 1)
+    #print(oil_price)   If you don't know return type, you have to remove comment and check the result
+    return oil_price
+
 def NameServiceAreaReader(serviceArea_name):
     url = 'https://data.ex.co.kr/openapi/business/serviceAreaRoute?key=' + api_key + '&type=xml&routeName=' + ex_name + '&numOfRows=50&pageNo=1'
     # url = 'https://data.ex.co.kr/openapi/specialAnal/trafficFlowByTime?key=5758303589&type=xml&iStdYear=2024'
@@ -62,4 +54,4 @@ def NameServiceAreaReader(serviceArea_name):
 def ExNameServiceAreaReader(ex_name):
     pass
 
-FoodMenuReader('죽암')
+GasstationReader('죽암(서울)')
