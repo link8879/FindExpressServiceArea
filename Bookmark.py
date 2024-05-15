885b99c77e1622e14f0b40d78bd53aa018d1f63b
Rungbam(main) <marungbam@naver.com>
2024-05-15 17:44:11 +0900

    클래스 별로 파일 분리

---
+++ Bookmark.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+class Bookmark(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = Label(self, text="즐겨찾기")
+        label.place(x=350, y=50)  # 중앙에 배치
+
+        HomeButton = Button(self, text="홈", command=lambda:
+                        controller.show_frame("HomePage"))
+        HomeButton.place(x=5, y=0)
+
+        OilButton = Button(self, text="주유소 가격 비교", command=lambda:
+                        controller.show_frame("OilPage"))
+        OilButton.place(x=5, y=200)
+
+        BookmarkButton = Button(self, text="즐겨찾기", command=lambda:
+                        controller.show_frame("Bookmark"))
+        BookmarkButton.place(x=5, y=400)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from HomePage import HomePage
+from OilPage import OilPage
+from Bookmark import Bookmark
 
 class MainGUI(Tk):
     def __init__(self):
@@ -24,66 +27,6 @@ class MainGUI(Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
-
-class HomePage(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-
-        label = Label(self, text="Home Page")
-        label.place(x=350, y=50)    # 중앙에 배치
-
-        HomeButton = Button(self, text="홈", command=lambda:
-                            controller.show_frame("HomePage"))
-        HomeButton.place(x=5, y=0)
-
-        OilButton = Button(self, text="주유소 가격 비교", command=lambda:
-                            controller.show_frame("OilPage"))
-        OilButton.place(x=5, y=200)
-
-        BookmarkButton = Button(self, text="즐겨찾기", command=lambda:
-                            controller.show_frame("Bookmark"))
-        BookmarkButton.place(x=5, y=400)
-
-class OilPage(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-
-        label = Label(self, text="Oil Page")
-        label.place(x=350, y=50)  # 중앙에 배치
-
-        HomeButton = Button(self, text="홈", command=lambda:
-                        controller.show_frame("HomePage"))
-        HomeButton.place(x=5, y=0)
-
-        OilButton = Button(self, text="주유소 가격 비교", command=lambda:
-                        controller.show_frame("OilPage"))
-        OilButton.place(x=5, y=200)
-
-        BookmarkButton = Button(self, text="즐겨찾기", command=lambda:
-                        controller.show_frame("Bookmark"))
-        BookmarkButton.place(x=5, y=400)
-
-class Bookmark(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-        self.controller = controller
-
-        label = Label(self, text="즐겨찾기")
-        label.place(x=350, y=50)  # 중앙에 배치
-
-        HomeButton = Button(self, text="홈", command=lambda:
-                        controller.show_frame("HomePage"))
-        HomeButton.place(x=5, y=0)
-
-        OilButton = Button(self, text="주유소 가격 비교", command=lambda:
-                        controller.show_frame("OilPage"))
-        OilButton.place(x=5, y=200)
-
-        BookmarkButton = Button(self, text="즐겨찾기", command=lambda:
-                        controller.show_frame("Bookmark"))
-        BookmarkButton.place(x=5, y=400)
 
 if __name__ == "__main__":
     app = MainGUI()
