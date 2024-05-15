5ae59654d7a7d458a0b70771db08b7f738800548
WonHyung Lee(Gram) <link8879@naver.com>
2024-05-15 17:27:47 +0900

    노선 별 모든 고속도로 휴게소 가져오기 성공

--- xmlReader.py
+++ xmlReader.py
@@ -45,13 +45,33 @@ def GasstationReader(serviceArea_name):
     #print(oil_price)   If you don't know return type, you have to remove comment and check the result
     return oil_price
 
-def NameServiceAreaReader(serviceArea_name):
-    url = 'https://data.ex.co.kr/openapi/business/serviceAreaRoute?key=' + api_key + '&type=xml&routeName=' + ex_name + '&numOfRows=50&pageNo=1'
-    # url = 'https://data.ex.co.kr/openapi/specialAnal/trafficFlowByTime?key=5758303589&type=xml&iStdYear=2024'
-    # url 불러오기
-    response = requests.get(url)
-    pass
-def ExNameServiceAreaReader(ex_name):
-    pass
+def AllServiceAreaReader(ex_name):      #
+    ex_code = ExnameToExcode(ex_name)
+    page_num = '1'
+    flag = False
+    service_area_list = []
+    while True:
+        url = 'https://data.ex.co.kr/openapi/locationinfo/locationinfoRest?key=' + api_key + '&type=xml&routeNo=' + ex_code + '&numOfRows=10&pageNo='+page_num
+        # url 불러오기
+        response = requests.get(url)
+        root = ET.fromstring(response.text)
+        flag = root.find("list")
+        if flag == None:
+            break
+        for list in root.iter("list"):
+            service_area_list.append(list.findtext("unitName"))
 
-GasstationReader('죽암(서울)')
+        page_num = str(int(page_num) + 1)
+    print(service_area_list)
+    return service_area_list
+
+def ExnameToExcode(ex_name):
+    url = 'https://data.ex.co.kr/openapi/business/curStateStation?key=test&type=xml&numOfRows=10&pageNo=1&routeName='+ex_name
+    response = requests.get(url)
+    root = ET.fromstring(response.text)
+    flag = root.find("list")
+    for list in root.iter("list"):
+        routeCode = list.findtext("routeCode")
+        return routeCode
+
+AllServiceAreaReader('경부선')
