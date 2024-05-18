ce0e9b278c5f5447502cc9f1fea96129a17924a1
Rungbam(main) <marungbam@naver.com>
2024-05-18 17:27:01 +0900

    주유소 가격비교 부분
    Entry, combobox추가

--- OilPage.py
+++ OilPage.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.ttk as ttk
 
 class OilPage(Frame):
     def __init__(self, parent, controller):
@@ -10,8 +11,19 @@ class OilPage(Frame):
         self.OilImage = PhotoImage(file="image/주유소 아이콘.png")
         self.BookmarkImage = PhotoImage(file="image/즐겨찾기(빈 별).png")
 
-        label = Label(self, text="Oil Page")
-        label.place(x=350, y=50)  # 중앙에 배치
+        RestAreas = [str(i) + "번 휴게소" for i in range(1, 101)]
+
+        # Entry를 이용해서 휴게소 검색창
+        RestAreas_Search_1 = Entry(self, width=33)
+        RestAreas_Search_1.place(x=200, y=100)
+        RestAreas_Search_2 = Entry(self, width=33)
+        RestAreas_Search_2.place(x=490, y=100)
+
+        # 휴게소 비교 콤보박스
+        RestAreas_List_1 = ttk.Combobox(self, width=30, height=10, values=RestAreas)
+        RestAreas_List_1.place(x=200, y=130)
+        RestAreas_List_2 = ttk.Combobox(self, width=30, height=10, values=RestAreas)
+        RestAreas_List_2.place(x=490, y=130)
 
         HomeButton = Button(self, image=self.HomeImage, width=100, height=100, command=lambda:
         controller.show_frame("HomePage"))
