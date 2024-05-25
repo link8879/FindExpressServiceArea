8d3297d2409212ae372a0036e622c7c271b4ab06
Rungbam(main) <marungbam@naver.com>
2024-05-25 16:54:09 +0900

    라벨 추가

--- OilPage.py
+++ OilPage.py
@@ -4,17 +4,23 @@ from tkinter import font
 from xmlReader import *
 
 class OilPage(Frame):
+    def TopText(self):
+        TempFont = font.Font(self, size=40, weight='bold', family='긱블말랑이')
+        MainText = Label(self, font=TempFont, text="휴게소 별 주유소 가격 비교")
+        MainText.pack()
+        MainText.place(x=190, y=20)
+
     def SearchButton_1st(self):
-        TempFont = font.Font(self, size=10, weight='bold', family='긱블말랑이')
+        TempFont = font.Font(self, size=10, family='긱블말랑이')
         SearchButton = Button(self, font=TempFont, text="검색")
         SearchButton.pack()
-        SearchButton.place(x=440, y=100)
+        SearchButton.place(x=440, y=110)
 
     def SearchButton_2nd(self):
-        TempFont = font.Font(self, size=10, weight='bold', family='긱블말랑이')
+        TempFont = font.Font(self, size=10, family='긱블말랑이')
         SearchButton = Button(self, font=TempFont, text="검색")
         SearchButton.pack()
-        SearchButton.place(x=740, y=100)
+        SearchButton.place(x=740, y=110)
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
@@ -28,18 +34,19 @@ class OilPage(Frame):
 
         # Entry를 이용해서 휴게소 검색창
         RestAreas_Search_1 = Entry(self, width=33, )
-        RestAreas_Search_1.place(x=200, y=100)
+        RestAreas_Search_1.place(x=200, y=110)
         RestAreas_Search_2 = Entry(self, width=33)
-        RestAreas_Search_2.place(x=490, y=100)
+        RestAreas_Search_2.place(x=490, y=110)
 
+        self.TopText()
         self.SearchButton_1st()
         self.SearchButton_2nd()
 
         # 휴게소 비교 콤보박스
         RestAreas_List_1 = ttk.Combobox(self, width=30, height=10, values=RestAreas)
-        RestAreas_List_1.place(x=200, y=130)
+        RestAreas_List_1.place(x=200, y=140)
         RestAreas_List_2 = ttk.Combobox(self, width=30, height=10, values=RestAreas)
-        RestAreas_List_2.place(x=490, y=130)
+        RestAreas_List_2.place(x=490, y=140)
 
         # 그림 넣을 캔버스
         self.MainCanvas = Canvas(self, width=150, height=150, bg='white')
