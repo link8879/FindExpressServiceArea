ef6e70f33717f69471edab53ed3b3e3cfc2c2384
WonHyung Lee(Gram) <link8879@naver.com>
2024-05-29 13:49:39 +0900

    폰트 조절

--- HomePage.py
+++ HomePage.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import tkinter.ttk as ttk
-from tkinter import PhotoImage
+from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import font
 import xmlReader as xml
@@ -11,6 +11,9 @@ class HomePage(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
+
+        self.default_font = font.Font(family="긱블말랑이", size=14)
+        self.larger_font = font.Font(family="긱블말랑이", size=20)
 
         self.HomeImage = PhotoImage(file="image/홈 아이콘.png")
         self.OilImage = PhotoImage(file="image/주유소 아이콘.png")
@@ -24,12 +27,12 @@ class HomePage(Frame):
 
         # highway route list
         # height = Number of times the list will display
-        self.Highway_Route_List = ttk.Combobox(self, width=50, height=10, values=self.Highway_Routes)
+        self.Highway_Route_List = ttk.Combobox(self, width=20, height=10, values=self.Highway_Routes,font=self.default_font)
         self.Highway_Route_List.place(x=285, y=100)
         self.Highway_Route_List.bind("<<ComboboxSelected>>", self.ComboBoxSelected)
 
         # RestArea list
-        self.RestArea_List = ttk.Combobox(self, width=50, height=10, values=self.ListOfRestAreas)
+        self.RestArea_List = ttk.Combobox(self, width=20, height=10, values=self.ListOfRestAreas,font = self.default_font)
         self.RestArea_List.place(x=285, y=130)
         self.RestArea_List.bind("<<ComboboxSelected>>",self.SecondComboBoxSelected)
 
@@ -65,43 +68,45 @@ class HomePage(Frame):
 
         self.map_widget = TkinterMapView(width=300, height=340, corner_radius=0)
         self.text_box = Text(self, width=40, height=26)
+
+
     def TopImage(self):
         MainCanvas = Canvas(self, width=150, height=150, bg='white')
         MainCanvas.pack()
         MainCanvas.place(x=0, y=0)
 
     def ComboBoxSelected(self,event):
-        print(xml.XmlReader.AllServiceAreaReader(self.Highway_Route_List.get()))
         self.ListOfRestAreas = xml.XmlReader.AllServiceAreaReader(self.Highway_Route_List.get())
         if len(self.ListOfRestAreas) == 0:
             self.RestArea_List['values'] = ['휴게소가 존재하지 않습니다']
         else:
             self.RestArea_List['values'] = self.ListOfRestAreas
     def SetTextBox(self):
-        default_font = font.Font(family="긱블말랑이", size=10)
-        larger_font = font.Font(family="긱블말랑이", size=15)
+
         self.text_box = Text(self, width=40, height=26)
+
         self.text_box.place(x=200, y=160)
         self.text_box.delete('1.0', END)
 
-        self.text_box.tag_configure("default", font=default_font)
-        self.text_box.tag_configure("large", font=larger_font)
+        self.text_box.tag_configure("default", font=self.default_font)
+        self.text_box.tag_configure("large", font=self.larger_font)
 
         info = xml.XmlReader.serviceAreaInfoReader(self.RestArea_List.get())  # adress parking up and down info
-        self.text_box.insert('1.0', '★주소★' + '\n', "large")
+        self.text_box.insert('1.0', '1.주소' + '\n', "large")
         self.text_box.insert('end', info['address'] + '\n', "default")
 
         food_menu = xml.XmlReader.FoodMenuReader(self.RestArea_List.get())  # food info
-        self.text_box.insert('end', '★음식 메뉴★' + '\n')
+        self.text_box.insert('end', '2.음식 메뉴' + '\n',"large")
         for food in food_menu:
-            self.text_box.insert('end', food + '\n')
+            self.text_box.insert('end', food + '\n',"default")
 
         # parking info
         # parking = xml.XmlReader.serviceAreaInfoReader(self.RestArea_List.get())
-        self.text_box.insert('end', '★주차 대수★' + '\n')
-        self.text_box.insert('end', '소형차 ' + str(info['small_parking']) + '\n')
-        self.text_box.insert('end', '대형차 ' + str(info['big_parking']) + '\n')
+        self.text_box.insert('end', '3.주차 대수' + '\n',"large")
+        self.text_box.insert('end', '소형차 ' + str(info['small_parking']) + '\n',"default")
+        self.text_box.insert('end', '대형차 ' + str(info['big_parking']) + '\n',"default")
 
+        self.text_box.config(state=DISABLED)
     def SetMap(self):
         info = xml.XmlReader.serviceAreaInfoReader(self.RestArea_List.get())
         self.map_widget = TkinterMapView(width=300, height=340, corner_radius=0)
